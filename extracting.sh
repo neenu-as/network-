fac90b61038a1dadada2bfc6dbf358e5489c82d6
NEENU AS <116252388+neenu-as@users.noreply.github.com>
2023-06-20 15:23:17 +0530

    Add files via upload

---
+++ extracting.sh
@@ -0,0 +1,18 @@
+#extracting a set of lines from set of lines
+if [ $# -le 2 ]
+then
+echo "syntax is <$0><range><file......>"
+exit 1
+fi
+n1=$1 
+n2=$2
+shift
+shift
+n3=`expr $n2 - $n1 + 1`
+while [ $# -ne 0 ]
+do
+head -$n2 $1 | tail -$n3
+shift
+done
+exit 0
+ 
